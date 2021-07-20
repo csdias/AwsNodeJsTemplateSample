69e0158738f8203bff35b418c53d68aba255daa2
Carlos Soares Dias <csdias@gmail.com>
2021-07-20 21:28:48 +0100

    setting config variables and calling dynamodb

--- user-service/src/functions/createUser.js
+++ user-service/src/functions/createUser.js
@@ -1,11 +1,32 @@
 'use strict';
 
 module.exports.createUser = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  const body = JSON.parse(event.body);
+  const username = body.username;
+  const password = body.password;
+  const newUserParams = {
+    TableName: process.env.DYNAMODB_USER_TABLE,
+    Item: {
+      pk: username,
+      password: password
+    }
+  }
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const putResult = await dynamodb.put(newUserParams).promise();
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credential': true,
+        'Access-Control-Allow-Headers': 'Authorization',
+      }
+    }
+  }
+  catch(putError) {
+    console.log('There was an error putting the new item');
+    console.log('putError', putError);
+    console.log('newUserParams', newUserParams);
+    return new Error('There was an error putting the new item');
+  }
 };
